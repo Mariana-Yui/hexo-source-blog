69622750f01734d01203c9757e27a9236d9a9b12
denislin <denislin@tencent.com>
2022-09-27 01:14:58 +0800

    chore: 调整live2d bottom

--- source/js/style.js
+++ source/js/style.js
@@ -4,6 +4,31 @@ if (["/archives/", "/tags/"].includes(location.pathname)) {
   banner.style.backgroundPositionY = "20%";
 }
 
+// if (['/'].includes(location.pathname)) {
+//   setTimeout(() => {
+//     var headBanner = document.querySelector('.header-inner');
+//     var hdBannerHeight =
+//       headBanner && headBanner.getBoundingClientRect().height;
+//     var live2dWidget = document.getElementById('live2d-widget');
+//     if (headBanner && live2dWidget) {
+//       var bottom = live2dWidget.style.bottom;
+//       live2dWidget.style.display = 'none';
+//       live2dWidget.style.bottom = '-100%';
+//       live2dWidget.style.transition = 'bottom 500ms ease-in';
+//       function scrollcb(e) {
+//         console.log(window.scrollY);
+//         if (window.scrollY > hdBannerHeight) {
+//           live2dWidget.style.bottom = bottom + 'px';
+//         } else {
+//           live2dWidget.style.bottom = '-100%';
+//         }
+//       }
+//       window.addEventListener('scroll', scrollcb);
+//       scrollcb();
+//     }
+//   }, 1000);
+// }
+
 jQuery("figure.highlight").click(function (e) {
   if ($(e.target).hasClass("icon-arrowdown")) {
     $(e.target).removeClass("icon-arrowdown").addClass("icon-arrowup");
